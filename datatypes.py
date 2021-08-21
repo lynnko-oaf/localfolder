3cc13b826ce4df07be4685a27d45ad8b007272cd
Lynn Ko <newbies.org@gmail.com>
2021-08-21 11:43:33 +0630

    data_types added

---
+++ datatypes.py
@@ -0,0 +1,5 @@
+#thre are four basic data types in Python
+print("This is string" , type("Hello"))
+print("This is integer" , type(123))
+print("This is float" , type(1.2))
+print("This is Boolean" , type(True))
