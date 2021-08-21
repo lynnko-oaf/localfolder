6e044ee56a161e2c95ec0c9673c2657d4d3d5b1a
Lynn Ko <newbies.org@gmail.com>
2021-08-21 11:56:01 +0630

    data types rules added

--- note for python.py
+++ note for python.py
@@ -1,3 +1,6 @@
 
 print("String must be written between quotes, single or double quotes.")
 #line 1 comment text is removed
+print("Interger is a number without decimal.")
+print("Float is a number with decimal places.")
+print("Boolean is a state that shows 'True' or 'False'")
